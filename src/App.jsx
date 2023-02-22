a2a4c8c60fad1b6112acb83937f8f143daa4b71d
valen <valentin332@hotmail.es>
2023-02-21 23:32:42 -0300

    Commit 4 IIRC. Added expiration date/hour functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import { nanoid } from 'nanoid'
  export default function App() {
  const [newToDo, setNewToDo] = React.useState("")
  const [toDoList, setToDoList] = React.useState(JSON.parse(localStorage.getItem("list")) || "")
- const [editing, setEditing] = React.useState(false)
+ const [editing, setEditing] = React.useState(false);
  const dias = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"];
+
  React.useEffect(() => {
  const toDoListJson = JSON.stringify(toDoList);
  toDoListJson && localStorage.setItem("list", toDoListJson)
@@ -27,7 +28,7 @@ function update(event){
 
 function editItem(event, itemId){
 setEditing(true);
-const inputField = document.querySelector("#todo--input");
+const inputField = document.querySelector("#tarea");
 inputField.value = toDoList.find(toDoItem => toDoItem.id === itemId).value;
 setToDoList(prevList => {
   const newerList = prevList.map(toDoItem => toDoItem.id !== itemId ? {...toDoItem, checked: false} : {...toDoItem, checked:true})
@@ -42,27 +43,28 @@ function handleEdit(){
  })
 
  setEditing(false)
-document.getElementById("todo--input").value = ""
+  document.getElementById("todo--input").value = ""
 }
 
 function handleAdd(){
-  if(newToDo !== ""){
+  if(newToDo  !== ""){
 const fecha = new Date();
 const fechaDatos = `${dias[fecha.getDay() - 1 ]} ${fecha.getDate()}/${fecha.getMonth() + 1}/${fecha.getFullYear()} `
-
+const datosFechaExp = document.querySelector("#fechaExp");
+const datosHoraExp = document.querySelector("#horaExp");
     const newItem = {
     id: nanoid(),
     value: newToDo,
     checked: false,
-    fecha: fechaDatos
+    fecha: fechaDatos,
+    fechaDeExpiracion: datosFechaExp.value,
+    horaDeExpiracion: datosHoraExp.value
     };
   if(!toDoList){
     setToDoList([newItem])
     } else {
       setToDoList((prevList) => [...prevList, newItem])
     }
-    //cleanup
-    document.getElementById("todo--input").value = ""
   }
 }
 
@@ -75,6 +77,8 @@ const todoRender = toDoList ? toDoList.map((todo, index) => {
     deleteFunc = { (event) => deleteItem(event,todo.id)}
     editFunc = { (event) => editItem(event, todo.id)}
     fechaDeCreacion = {todo.fecha}
+    fechaDeExpiracion = {todo.fechaDeExpiracion}
+    horaDeExpiracion = {todo.horaDeExpiracion}
     />
     )
 }) : "";
@@ -84,10 +88,14 @@ const todoRender = toDoList ? toDoList.map((todo, index) => {
   <h1 className="main--header">TO DO LIST</h1>
     <form className='main--form mb-3'>
     <div className="form-floating">
-    <input className='form-control form-control-sm'   onChange={update}type="text" id="todo--input" placeholder='Add Task' ></input>
-    <label className='form-label' htmlFor="#todo--input">Add new task</label>
+    <input className='form-control form-control-sm'   onChange={update} type="text" id="tarea" placeholder='Add Task'/>
+    <label className='form-label' htmlFor="#tarea">Añadir nueva tarea</label>
     </div>
-    <button className='mt-1'  onClick={ editing ? handleEdit : handleAdd}>{editing ? "Submit" : "Add"}</button>
+    <label htmlFor="#fechaExp">Fecha de Expiración (Opcional)</label>
+    <input className='form-control form-control-sm'   type="date"  name="fechaDeExpiracion" id="fechaExp" /> 
+    <input className='form-control form-control-sm'   type="time" name="horaDeExp" id="horaExp"/>
+    <br/>
+    <button className='mt-1'  onClick={ editing ? handleEdit : handleAdd}>{editing ? "Confirmar" : "Añadir"}</button>
 </form>
 
    {todoRender}
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -6,6 +6,9 @@ return (
 <div  className="todo--item mb-2">
 <h3>{props.val}</h3>
 <p>Fecha de creación : {props.fechaDeCreacion}</p>
+
+{props.fechaDeExpiracion && <p>Fecha de expiración : {props.fechaDeExpiracion} {props.horaDeExpiracion && `a las ${props.horaDeExpiracion}`}</p>}
+{props.fechaDeExpiracion && <p>Estado : Pendiente</p>}
 <FontAwesomeIcon className="btn btn-danger btn-lg" onClick={props.deleteFunc} icon= {faXmarkSquare} />
 <FontAwesomeIcon className="btn btn-success btn-lg" onClick={props.deleteFunc} icon= {faCheckSquare} />
 <button type="button" onClick={props.editFunc} className="btn btn-primary btn">Edit</button>
